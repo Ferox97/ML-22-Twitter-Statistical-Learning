86e28f785daf63bf9c3c9821fa90cad8108f6762
Ferox97 <37930979+Ferox97@users.noreply.github.com>
2022-10-09 20:32:18 +0200

    Problem Twitter API Max Lenght
    To be fixed

--- TwitterAPI/src/main/java/com/twitterAPI/TwitterAPI/GoogleNews.java
+++ TwitterAPI/src/main/java/com/twitterAPI/TwitterAPI/GoogleNews.java
@@ -26,6 +26,11 @@ public class GoogleNews {
 		Elements newsLinks = doc.select("rss channel item link");
 
 		for(int i = 0; i < 10; i++) {   // title.size()  
+			
+			// TO:DO 
+			// SE LINK NOTIZIA HA PIU' DI 128 CARATTERI (https:// escluso) non viene accettato dalla Query delle Twitter API
+			// MI DEVO INVENTARE QUALCOSA (NON POSSO FARE CUT RANDOM)
+			// PROBABILMENTE LO ESCLUDO E MI PRENDO SOLAMENTE I LINK VALIDI ENTRO IL LIMITE DI CHAR 
 
 			String temp = newsLinks.get(i).toString();
 			String tempDue = temp.substring(6 , temp.length() - 7); //Rimuovo i tag HTML <link> e </link>
@@ -38,7 +43,7 @@ public class GoogleNews {
 
 	}
 
-	public static String getFinalURL(String url) throws IOException , MalformedURLException { //Lo uso per avere i redirect finali dei link di google news
+	public static String getFinalURL(String url) throws IOException , MalformedURLException { // metodo Per avere i redirect finali dei link di google news
 		HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
 		con.setInstanceFollowRedirects(false);
 		con.connect();
